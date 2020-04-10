59088a82e9988ee07d02036f7526aeeaa9ba758e
Bousquet <bousquet.laurent12@gmail.com>
2020-04-10 23:40:02 +0200

    [DOC] Readme added [TEST] Add python coverage and first test

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # lib.s_utils
+
+## Installation
+
+### Installing virtual-env
+
+On ubuntu
+
+```
+sudo apt install vitualenv
+```
+
+### Create environment
+
+```
+virtualenv --python=python3 venv
+```
+
+## Activate environment
+
+```
+source venv/bin/activate
+```
+
---
+++ s_utils/dict.py
@@ -0,0 +1,19 @@
+def get_key(content, key, logger = None):
+    '''Check is a key value is present or not in a dictionary.
+       if logger is set the value will be logged in info is present
+       and warning if not present.'''
+    if not content:
+        if logger: logger.warning('Empty dictionary detected')
+        return None
+    if not isinstance(content, dict):
+        if logger: logger.warning('Invalid dictionary detected')
+        return None
+
+    if not key in dict_content:
+        if logger: logger.warning('key {} was not found in the dictionary'.format(key))
+        return None
+    else:
+        value = content[key]
+        if logger: logger.info('key "{}" = "{}"'.format(key, value))
+        return value
+
