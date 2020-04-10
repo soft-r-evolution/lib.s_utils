a13ed2f4e68c0d0752b00a9429f222355a146d32
Bousquet <bousquet.laurent12@gmail.com>
2020-04-11 01:01:19 +0200

    [QA] Black refact

--- tests/dict_test.py
+++ tests/dict_test.py
@@ -5,22 +5,24 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+
 def test_get_key():
     assert not get_key(None, None)
     assert not get_key(2, None)
-    assert not get_key(2, 'value')
+    assert not get_key(2, "value")
     test_dict = dict()
-    test_dict['a'] = 12
-    test_dict['b'] = 2
-    assert not get_key(test_dict, 'c')
-    assert get_key(test_dict, 'a') == 12
+    test_dict["a"] = 12
+    test_dict["b"] = 2
+    assert not get_key(test_dict, "c")
+    assert get_key(test_dict, "a") == 12
+
 
 def test_get_key_logger():
     assert not get_key(None, None, logger)
     assert not get_key(2, None, logger)
-    assert not get_key(2, 'value', logger)
+    assert not get_key(2, "value", logger)
     test_dict = dict()
-    test_dict['a'] = 12
-    test_dict['b'] = 2
-    assert not get_key(test_dict, 'c', logger)
-    assert get_key(test_dict, 'a', logger) == 12
+    test_dict["a"] = 12
+    test_dict["b"] = 2
+    assert not get_key(test_dict, "c", logger)
+    assert get_key(test_dict, "a", logger) == 12
