8df9134481f54d99676db9e8ccced8472c31deb2
Bousquet <bousquet.laurent12@gmail.com>
2020-04-11 00:17:40 +0200

    [QA] Black processed files

--- s_utils/dict.py
+++ s_utils/dict.py
@@ -1,19 +1,22 @@
-def get_key(content, key, logger = None):
-    '''Check is a key value is present or not in a dictionary.
+def get_key(content, key, logger=None):
+    """Check is a key value is present or not in a dictionary.
        if logger is set the value will be logged in info is present
-       and warning if not present.'''
+       and warning if not present."""
     if not content:
-        if logger: logger.warning('Empty dictionary detected')
+        if logger:
+            logger.warning("Empty dictionary detected")
         return None
     if not isinstance(content, dict):
-        if logger: logger.warning('Invalid dictionary detected')
+        if logger:
+            logger.warning("Invalid dictionary detected")
         return None
 
     if not key in content:
-        if logger: logger.warning('key {} was not found in the dictionary'.format(key))
+        if logger:
+            logger.warning("key {} was not found in the dictionary".format(key))
         return None
     else:
         value = content[key]
-        if logger: logger.info('key "{}" = "{}"'.format(key, value))
+        if logger:
+            logger.info('key "{}" = "{}"'.format(key, value))
         return value
-
--- tests/dict_test.py
+++ tests/dict_test.py
@@ -2,5 +2,6 @@ from s_utils.dict import get_key
 
 import pytest
 
+
 def test_get_key():
     assert get_key(None, None) == None
