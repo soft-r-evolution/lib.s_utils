180bf9a990315072267b9891cd814328476b3a4a
Bousquet <bousquet.laurent12@gmail.com>
2020-04-11 22:45:24 +0200

    [FEAT] Existing seems to work

--- s_utils/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-
-def get_logger():
-    """ Return a s_utils logger """
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    return logger
--- tests/dict_test.py
+++ tests/dict_test.py
@@ -1,11 +1,5 @@
 from s_utils.dict import get_key
 
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-
 def test_get_key():
     assert not get_key(None, None)
     assert not get_key(2, None)
@@ -18,11 +12,11 @@ def test_get_key():
 
 
 def test_get_key_logger():
-    assert not get_key(None, None, logger)
-    assert not get_key(2, None, logger)
-    assert not get_key(2, "value", logger)
+    assert not get_key(None, None)
+    assert not get_key(2, None)
+    assert not get_key(2, "value")
     test_dict = dict()
     test_dict["a"] = 12
     test_dict["b"] = 2
-    assert not get_key(test_dict, "c", logger)
-    assert get_key(test_dict, "a", logger) == 12
+    assert not get_key(test_dict, "c")
+    assert get_key(test_dict, "a") == 12
